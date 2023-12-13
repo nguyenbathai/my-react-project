4d61375fca3a2a12b2ac68d5237e5c2693b15e64
alan-videa <alan@videabiz.com>
2023-12-14 01:08:13 +0700

    alan commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import ConnectButton from "./button";
+
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+import { WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet } from 'viem/chains'
+import Web3Modal from "./_app";
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = 'd7ba718612de445bd528f93351d24fc1'
+
+// 2. Create wagmiConfig
+const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains })
 
 function App() {
   return (
     <div className="App">
+      <ConnectButton></ConnectButton>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          <WagmiConfig config={wagmiConfig}>
+              <Component {...pageProps} />
+          </WagmiConfig>
+          <Web3Modal
+            projectId={projectId}
+            theme={"dark"}
+          />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
