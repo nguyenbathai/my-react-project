6b192fc5df718ceacf3797ffad96aedb84071d8f
nguyenbathai <nguyenbathai.nbt@gmail.com>
2023-12-15 21:34:30 +0700

    alan commit

--- src/component.js
+++ src/component.js
@@ -4,6 +4,7 @@ import { WagmiConfig } from 'wagmi'
 import {arbitrum, baseGoerli, mainnet} from 'viem/chains'
 import {Profile} from "./wallet";
 import {Send} from "./send";
+import ReadContact from "./readContact";
 
 // 1. Get projectId at https://cloud.walletconnect.com
 const projectId = 'd7ba718612de445bd528f93351d24fc1'
@@ -16,7 +17,7 @@ const metadata = {
     icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-const chains = [baseGoerli]
+const chains = [mainnet,arbitrum]
 const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
 
 // 3. Create modal
@@ -24,10 +25,14 @@ createWeb3Modal({ wagmiConfig, projectId, chains })
 
 export default function Web3Modal() {
     return (
-        <WagmiConfig config={wagmiConfig}>
-            <Profile/>
-            <Send/>
+        <>
+            <WagmiConfig config={wagmiConfig}>
+                <Profile/>
+                <Send/>
+                <ReadContact/>
+            </WagmiConfig>
+        </>
 
-        </WagmiConfig>
+
     )
 }
